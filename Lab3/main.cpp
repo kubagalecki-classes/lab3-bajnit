1c610b543f666b891f753f593a45b4d3087de399
bajnit <krzysiek.kallas@me.com>
2020-11-12 13:56:04 +0100

    Initial Commit

---
+++ Lab3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab3
+//
+//  Created by Krzysztof Kallas on 12/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
