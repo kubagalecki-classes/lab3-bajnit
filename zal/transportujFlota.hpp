e22ca50c1732036bf0ab73dead74566bb5ce78b3
bajnit <krzysiek.kallas@me.com>
2020-11-25 22:56:57 +0000

    p_domowa

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,23 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int towar_transportowany = 0;
+    unsigned int zaglowce = 0;
+    
+    if(towar == 0){
+      return zaglowce;
+    }
+
+    while (towar_transportowany<towar){
+      Statek* statek = stocznia();
+      towar_transportowany += (statek -> transportuj());
+
+      if(dynamic_cast<Zaglowiec*>(statek)){
+        zaglowce++;
+      }
+      delete statek;
+
+    }
+      
+    return zaglowce;
 }
