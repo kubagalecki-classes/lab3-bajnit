9532bb222e979b54bf2b85163c15d3e7004d55cf
Jakub Gałecki <33153107+kubagalecki@users.noreply.github.com>
2020-11-26 11:49:41 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -10,9 +10,12 @@ unsigned int transportujFlota(unsigned int towar)
     if(towar == 0){
       return zaglowce;
     }
+    
+    // Brakowało deklaracji stoczni
+    Stocznia stocznia;
 
     while (towar_transportowany<towar){
-      Statek* statek = Stocznia();
+      Statek* statek = stocznia();
       towar_transportowany += (statek -> transportuj());
 
       if(dynamic_cast<Zaglowiec*>(statek)){
